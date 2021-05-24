76cf65ad3bdedfaef01a46acc1683c6679e8c804
Onur Caglayan <62426634+onurmx@users.noreply.github.com>
2021-05-24 17:38:37 +0300

    Clipping from scratch

--- Rasterization/Clipping Algorithm/Clipping.cs
+++ Rasterization/Clipping Algorithm/Clipping.cs
@@ -9,23 +9,12 @@ namespace Rasterization
 {
     public class Clipping
     {
-        private int[] CalculateNumerators(int x0, int y0, int xMin, int yMin, int xMax, int yMax)
-        {
-            int q1 = x0 - xMin;
-            int q2 = xMax - x0;
-            int q3 = y0 - yMin;
-            int q4 = yMax - y0;
-            int[] returnArray = { q1, q2, q3, q4 };
-            return returnArray;
-        }
+        private float dx { get; set; }
+        private float dy { get; set; }
+        private float tE { get; set; }
+        private float tL { get; set; }
 
-        private int[] CalculateDenominators(int x0, int y0, int x1, int y1)
-        {
-            int dX = x1 - x0;
-            int dY = y1 - y0;
-            int[] returnArray = { -dX, dX, -dY, dY };
-            return returnArray;
-        }
+
 
         public Image LiangBarskyClipping(Point p1, Point p2, Rectangle clip, Image image)
         {
@@ -42,80 +31,72 @@ namespace Rasterization
             int xMax = clip.TopLeft.X >= clip.BottomRight.X ? clip.TopLeft.X : clip.BottomRight.X;
             int yMax = clip.TopLeft.Y >= clip.BottomRight.Y ? clip.TopLeft.Y : clip.BottomRight.Y;
 
-            double tE = 0.0;
-            double tL = 1.0;
+            dx = p2.X - p1.X;
+            dy = p2.Y - p1.Y;
 
-            if ((xMin < p1.X && p1.X < xMax) &&
-                (yMin < p1.Y && p1.Y < yMax) &&
-                (xMin < p2.X && p2.X < xMax) &&
-                (yMin < p2.Y && p2.Y < yMax))
+            tE = 0;
+            tL = 1;
+
+            if (clipT(xMin - p1.X, dx))
             {
-                newLine.StartPoint = new Point(p1.X, p1.Y);
-                newLine.EndPoint = new Point(p2.X, p2.Y);
-
-                lineDrawing.lineDDA(newLine, bitmap);
-                return bitmap;
-            }
-
-            var Denominators = CalculateDenominators(p1.X, p1.Y, p2.X, p2.Y);
-            var Numerators = CalculateNumerators(p1.X, p1.Y, xMin, yMin, xMax, yMax);
-
-            List<double> lessThanZeroList = new List<double>();
-            List<double> biggerThanZeroList = new List<double>();
-            lessThanZeroList.Add(tE);
-            biggerThanZeroList.Add(tL);
-
-            for (int i = 0; i < Denominators.Length; i++)
-            {
-                double t = (double)Numerators[i] / (double)Denominators[i];
-                if (Denominators[i] < 0)
+                if (clipT(p1.X - xMax, -dx))
                 {
-                    lessThanZeroList.Add(t);
-                }
-                else
-                {
-                    biggerThanZeroList.Add(t);
-                }
-            }
-
-            double newtE = lessThanZeroList.Max();
-            double newtL = biggerThanZeroList.Min();
-
-            if (tE != newtE && tL == newtL)
-            {
-                int x1 = p1.X + (int)(newtE * (p2.X - p1.X));
-                int y1 = p1.Y + (int)(newtE * (p2.Y - p1.Y));
-
-                newLine.StartPoint = new Point(x1, y1);
-                newLine.EndPoint = new Point(p2.X, p2.Y);
-
-                lineDrawing.lineDDA(newLine, bitmap);
-            }
-            else if (tE == newtE && tL != newtL)
-            {
-                int x2 = p2.X + (int)(newtL * (p2.X - p1.X));
-                int y2 = p2.Y + (int)(newtL * (p2.Y - p1.Y));
-
-                newLine.StartPoint = new Point(p1.X, p1.Y);
-                newLine.EndPoint = new Point(x2, y2);
-
-                lineDrawing.lineDDA(newLine, bitmap);
-            }
-            else if (tE != newtE && tL != newtL)
-            {
-                int x1 = p1.X + (int)(newtE * (float)(p2.X - p1.X));
-                int y1 = p1.Y + (int)(newtE * (float)(p2.Y - p1.Y));
-                int x2 = p1.X + (int)(newtL * (float)(p2.X - p1.X));
-                int y2 = p1.Y + (int)(newtL * (float)(p2.Y - p1.Y));
-                if ((x1 < bitmap.Width && x2 < bitmap.Width) && (y1 < bitmap.Height && y2 < bitmap.Height))
-                {
-                    newLine.StartPoint = new Point(x1, y1);
-                    newLine.EndPoint = new Point(x2, y2);
-
-                    lineDrawing.lineDDA(newLine, bitmap);
+                    if (clipT(yMin - p1.Y, dy))
+                    {
+                        if (clipT(p1.Y - yMax, -dy))
+                        {
+                            if (tL < 1)
+                            {
+                                newLine.EndPoint = new Point((int)((float)p1.X + tL * dx), (int)((float)p1.Y + tL * dy));
+                            }
+                            else //My improvement
+                            {
+                                newLine.EndPoint = p2;
+                            }
+                            if (tE > 0)
+                            {
+                                newLine.StartPoint = new Point((int)((float)p1.X + (tE * dx)), (int)((float)p1.Y + (tE * dy)));
+                            }
+                            else //My improvement
+                            {
+                                newLine.StartPoint = p1;
+                            }
+                        }
+                    }
                 }
             }
+
+            lineDrawing.lineDDA(newLine, bitmap);
+
             return bitmap;
+        }
+
+        public bool clipT(float numer, float denom)
+        {
+            if (denom == 0)
+            {
+                if (numer > 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            float t = numer / denom;
+            if (denom > 0)
+            {
+                if (t > tL)
+                    return false;
+                if (t > tE)
+                    tE = t;
+            }
+            else
+            {
+                if (t < tE)
+                    return false;
+                if (t < tL)
+                    tL = t;
+            }
+            return true;
         }
     }
 }
